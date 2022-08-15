db097b05da350feb05f58d90b5f169a0b01c320c
Robert <robert.ios.developer@gmail.com>
2022-08-14 22:37:12 -0600

    :fire: table view data source

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var names = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "The list"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+    
+    @IBOutlet var addButton: UIBarButtonItem!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    @IBAction func addButton(_ sender: Any) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) {
+            [unowned self] action in
+            
+            guard let textField = alert.textFields?.first,
+                  let nameToSave = textField.text else {
+                return
+            }
+            self.names.append(nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath )
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
